fa2083c7a160b36cb93fb501e0529a006e29ea06
jaeyeon.kim <foodsksms@gmail.com>
2024-12-01 00:49:34 +0900

    블로그 테스트 코드 보완

--- src/test/kotlin/me/jimmy/blogsource/web/storage/BlogTestJdbcRepositoryTest.kt
+++ src/test/kotlin/me/jimmy/blogsource/web/storage/BlogTestJdbcRepositoryTest.kt
@@ -1,5 +1,6 @@
 package me.jimmy.blogsource.web.storage
 
+import me.jimmy.blogsource.ComparisonUtils
 import me.jimmy.blogsource.IntegrationTest
 import me.jimmy.blogsource.web.storage.entity.blog.BlogTestEntity
 import me.jimmy.blogsource.web.storage.entity.blog.BlogTestJdbcRepository
@@ -11,33 +12,8 @@ import org.junit.jupiter.api.assertDoesNotThrow
 internal class BlogTestJdbcRepositoryTest(
     private val sut: BlogTestJdbcRepository,
 ) {
-
     @Test
     fun saveTest() {
-        val testString = "testString"
-        val entity = BlogTestEntity(
-            testString = testString,
-            testBoolean = null,
-            testInt = null,
-            testLong = null,
-            testBigDecimal = null,
-            testDouble = null,
-            testFloat = null,
-            testByte = null,
-            testShort = null
-        )
-
-        assertDoesNotThrow { sut.save(entity) }
-
-        // then
-        val resultList = assertDoesNotThrow { sut.findByTestString(testString) }
-        assertTrue(resultList.isNotEmpty())
-
-        sut.deleteById(resultList.first().id!!)
-    }
-
-    @Test
-    fun saveTest2() {
         val entity = BlogTestEntity(
             testString = null,
             testBoolean = true,
@@ -55,5 +31,25 @@ internal class BlogTestJdbcRepositoryTest(
         // then
         val resultList = assertDoesNotThrow { sut.findAll() }
         assertTrue(resultList.isNotEmpty())
+        val result = resultList.first()
+
+        ComparisonUtils.isEqualsTo(
+            result,
+            BlogTestEntity(
+                testString = null,
+                testBoolean = false,
+                testInt = 0,
+                testLong = 0L,
+                testBigDecimal = null,
+                testDouble = 0.0,
+                testFloat = 0F,
+                testByte = 0,
+                testShort = 0
+            ),
+            ignoreFields = listOf("id")
+        )
+
+
+        sut.deleteById(result.id!!)
     }
 }
